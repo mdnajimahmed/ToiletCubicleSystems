03e55d4f374e9802ce448ef3d12966d9967b2e4a
Md Najim Ahmed <najim.ju@gmail.com>
2023-10-21 21:45:04 +0800

    kafka producer ready

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import {Server} from './src/interfaces/server.js';
 import * as routes from './src/controllers/routes.index.js';
 import {addNewUser} from './src/service/train.js'
 import {detectHuman} from './src/service/detectHuman.js';
+import {produce} from './src/service/kafkaProducer.js';
 
 
 export const api = async (event, context) => {
@@ -13,7 +14,18 @@ export const train = async (event, context) => {
     await addNewUser(event)
 };
 export const detect = async (event, context) => {
-    await detectHuman(event)
+    // await detectHuman(event)
+    const kInput = {
+        topic: 'alowl',
+        messages: [
+          {
+            value: 'Hello, world!'
+          },
+        ],
+      }
+      console.log("producting",kInput)
+      const response = await produce(kInput)
+      console.log("response",response)
 };
 
 
--- backend/src/service/kafkaProducer.js
+++ backend/src/service/kafkaProducer.js
@@ -1,28 +1,35 @@
-import {Kafka} from 'kafkajs';
-const { IAMAuth } = require('@jm18457/kafkajs-msk-iam-authentication-mechanism');
+import { Kafka } from 'kafkajs';
+// import { IAMAuth } from '@jm18457/kafkajs-msk-iam-authentication-mechanism';
+
+// const kafka = new Kafka({
+//     brokers: ['boot-9ixeduhy.c3.kafka-serverless.ap-southeast-1.amazonaws.com:9098'],
+//     authenticationMechanism: new IAMAuth({
+//         region: 'ap-southeast-1',
+//         mwsRoleArn: 'arn:aws:iam::367739249270:role/toilet-cubicle-system-dev-ap-southeast-1-lambdaRole',
+//     }),
+// });
+
+import {createMechanism } from '@jm18457/kafkajs-msk-iam-authentication-mechanism';
 
 const kafka = new Kafka({
-    brokers: ['boot-9ixeduhy.c3.kafka-serverless.ap-southeast-1.amazonaws.com:9098'],
-    authenticationMechanism: new IAMAuth({
-      region: 'ap-southeast-1',
-      mwsRoleArn: 'arn:aws:iam::367739249270:role/toilet-cubicle-system-dev-ap-southeast-1-lambdaRole',
-    }),
-  });
-  
-  const producer = kafka.producer();
-  
-  const run = async () => {
-    await producer.connect();
-    await producer.send({
-      topic: '<YOUR_TOPIC>',
-      messages: [
-        {
-          value: 'Hello, world!',
-        },
-      ],
-    });
-  
-    await producer.disconnect();
-  };
-  
-  run().catch(console.error);
+  brokers: ['boot-9ixeduhy.c3.kafka-serverless.ap-southeast-1.amazonaws.com:9098'],
+  clientId: 'aLowL',
+  ssl: true,
+  sasl: createMechanism({ region: 'ap-southeast-1' })
+});
+
+const producer = kafka.producer();
+
+export const produce = async (topic, messages) => {
+    try {
+        console.log("connecting with kafka")
+        await producer.connect();
+        console.log("connected")
+        const response = await producer.send(topic, messages);
+        await producer.disconnect();
+        return response;
+    } catch (e) {
+        console.log("Error", e)
+        throw e
+    }
+};
